6b08f69b5bc476222bc0de97a2395d277f4813d0
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2026-03-29 16:51:15 +0300

    add dragon matching logic

--- dragons/views.py
+++ dragons/views.py
@@ -4,9 +4,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView
-
+from potions.models import Potion
 from dragons.models import Dragon
 
+from potions.choices import POTION_RECIPES
 
 # Create your views here.
 
@@ -17,18 +18,43 @@ class DisplayDragonView(ListView):
     paginate_by = 3
 
 
-class RandomDragonView(LoginRequiredMixin,View):
+
+
+class RandomDragonView(LoginRequiredMixin, View):
 
     def get(self, request):
-        dragons= list(Dragon.objects.all())
+        user = request.user
+        
+        # Check if the user already has a dragon
+        dragon = Dragon.objects.filter(rider=user).first()
+        if dragon:
+            return render(request, 'dragons/display-random-page.html', {'dragon': dragon, 'newly_matched': False})
 
-        if not dragons:
-            context = {'dragons':None}
-            return render(request,'dragons/display-random-page.html',context)
+        # Logic to check if they have found all potions
+        # total_recipes_count = len(set(POTION_RECIPES.values()))
+        total_recipes_count=2 #todo to make it more
+        user_discovered_count = Potion.objects.filter(magician=user).values('name').distinct().count()
+        
+        can_match = user_discovered_count >= total_recipes_count
+        
+        if not can_match:
+            context = {
+                'can_match': False,
+                'total_needed': total_recipes_count,
+                'user_has': user_discovered_count
+            }
+            return render(request, 'dragons/display-random-page.html', context)
 
-        random_dragon = random.choice(dragons)
-        context = {'dragon':random_dragon}
-        return render(request,'dragons/display-random-page.html',context)
+        # If they are worthy, find a dragon
+        available_dragons = list(Dragon.objects.filter(rider__isnull=True))
+        
+        if available_dragons:
+            dragon = random.choice(available_dragons)
+            dragon.rider = user
+            dragon.save()
+            return render(request, 'dragons/display-random-page.html', {'dragon': dragon, 'newly_matched': True, 'can_match': True})
+        else:
+            return render(request, 'dragons/display-random-page.html', {'dragon': None, 'can_match': True})
 
 
 
